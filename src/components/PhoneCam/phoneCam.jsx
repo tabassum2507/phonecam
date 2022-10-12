bb3da6bd7fa9d861e3b1097c6f004a57cf4d39af
tabassum2507 <10tabassum2@gmail.com>
2022-10-12 12:48:27 +0530

    back camera

--- src/components/PhoneCam/phoneCam.jsx
+++ src/components/PhoneCam/phoneCam.jsx
@@ -1,21 +1,49 @@
-import React, { useState } from 'react';
-import Camera from 'react-html5-camera-photo';
+import React from 'react';
+import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 
-import ImagePreview from './imagePreview'; 
 function PhoneCam (props) {
   function handleTakePhoto (dataUri) {
     // Do stuff with the photo...
     console.log('takePhoto');
   }
 
+  function handleTakePhotoAnimationDone (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto');
+  }
+
+  function handleCameraError (error) {
+    console.log('handleCameraError', error);
+  }
+
+  function handleCameraStart (stream) {
+    console.log('handleCameraStart');
+  }
+
+  function handleCameraStop () {
+    console.log('handleCameraStop');
+  }
+
   return (
     <Camera
       onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+      onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
+      onCameraError = { (error) => { handleCameraError(error); } }
+      idealFacingMode = {FACING_MODES.ENVIRONMENT}
+      idealResolution = {{width: 640, height: 480}}
+      imageType = {IMAGE_TYPES.JPG}
+      imageCompression = {0.97}
+      isMaxResolution = {true}
+      isImageMirror = {false}
+      isSilentMode = {false}
+      isDisplayStartCameraError = {true}
+      isFullscreen = {false}
+      sizeFactor = {1}
+      onCameraStart = { (stream) => { handleCameraStart(stream); } }
+      onCameraStop = { () => { handleCameraStop(); } }
     />
   );
 }
 
-
-
-export default PhoneCam
+export default PhoneCam;
